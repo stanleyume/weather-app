c2aab9a282851a6d79b4d19d6ad907f242ef3497
Stanley <coolstanlee@gmail.com>
2019-02-01 23:57:48 +0100

    Started the Day component

---
+++ src/components/Day.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class Day extends Component {
+  render(){
+    return (<div>Day</div>);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Day from './components/Day';
 
-ReactDOM.render(<div>Hello lol</div>, document.getElementById('app'));
+ReactDOM.render(<div>Hello world</div>, document.getElementById('app'));
