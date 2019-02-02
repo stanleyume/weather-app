d16b47f7c1c2646cceda8319df68771f20b3740a
Stanley <coolstanlee@gmail.com>
2019-02-02 23:35:02 +0100

    Loaders e.t.c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Day from './components/Day';
+require('../public/css/weather-icons.min.css');
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById('app'));
+ReactDOM.render(<div><i class="wi wi-day-sunny"></i></div>, document.getElementById('app'));
