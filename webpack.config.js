0d7cce2693737ef881cae0ba1d97ad4578a7ccfa
Stanley <coolstanlee@gmail.com>
2019-02-02 23:34:37 +0100

    Restructuring

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,22 @@ const path = require('path');
 module.exports = {
   devServer: {
     host: 'localhost',
-    port: '5000'
+    port: '5000',
+    compress: true,
+    contentBase: path.join(__dirname, 'public')
   },
 
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.min.js'
+    path: path.join(__dirname, 'public'),
+    filename: 'js/app.js',
+    // publicPath: '/public/'
   },
 
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         // use: {
@@ -24,6 +27,16 @@ module.exports = {
         //     presets: ['babel-preset-env', 'babel-preset-react']
         //   }
         // }
+      },
+      {
+        test: /\.(css|sass)$/,
+        loader: 'style-loader!css-loader!sass-loader'
+        // loader: 'style-loader!css-loader'
+        // use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)$/,
+        loader: 'file-loader'
       }
     ]
   }
