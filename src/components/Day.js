76e6265491386d68fb679f7a0ee375d18c526ed8
Stanley <coolstanlee@gmail.com>
2019-02-03 08:01:47 +0100

    Styling changes & title

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 class Day extends Component {
   render(){
-    return (<div>Day</div>);
+    return (
+      // <span>
+        <div className="card">
+          <div>{this.props.day}</div>
+          <br/>
+          <i className="wi wi-day-cloudy"></i>
+          <br/>
+          <div style={{ marginTop: '5px' }}>
+            <span>{this.props.t1}</span>
+            <span style={{ marginLeft: '10px' }}>{this.props.t2}</span>
+          </div>
+        </div>
+      // </span>
+    );
   }
 }
+
+export default Day;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Day from './components/Day';
 require('../public/css/weather-icons.min.css');
+require('../public/css/style.css');
 
-ReactDOM.render(<div><i class="wi wi-day-sunny"></i></div>, document.getElementById('app'));
+ReactDOM.render(
+  <div style={{ textAlign: 'center' }}>
+    <h1 className="title">Weather Man</h1>
+
+    <Day day="Mon" t1="70" t2="68"/>
+    <Day day="Tue"/>
+    <Day day="Wed"/>
+    <Day day="Thur"/>
+    <Day day="Fri"/>
+    <Day day="Sat"/>
+    <Day day="Sun"/>
+  </div>
+, document.getElementById('app'));
